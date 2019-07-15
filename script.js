58095df5cc4a9b3bf6bd218e1939b4ccbe3a5b30
Shervin Firouzdehghan <51175591+shervinf1@users.noreply.github.com>
2019-07-15 14:53:20 -0400

    V 1.5.8

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("menu").style.top = "0";
   } else {
-    document.getElementById("menu").style.top = "-73px";
+    document.getElementById("menu").style.top = "-74px";
   }
 }
